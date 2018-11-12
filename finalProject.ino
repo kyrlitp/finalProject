56a3ee2abe0dd428a874b1b711ef9f9dac84920d
Jalen <kyrlitp@gmail.com>
2018-11-12 14:57:56 -0500

    Update finalProject.ino
    added another dimension to array hardCodedSequence -- next will add a random button to randomize groove amnt and pattern.

--- finalProject.ino
+++ finalProject.ino
@@ -1,3 +1,5 @@
+#include <MIDI.h>
+
 int tempo = 200; //this is in ms
 int grooveAmount = 50;
 int currentStep = 0;
@@ -6,10 +8,22 @@ unsigned long lastStepTime = 0;
 unsigned long nextStepTime = 0;
 // int acceptableMidiValues[] = {} // need to limit this to 36 to 51 (C1 to D#2)
 
-boolean hardCodedSequence[3][8] = {
-  { HIGH, LOW, LOW, LOW, LOW, HIGH, LOW, LOW }, //KICK
-  { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, //HAT
-  { LOW, LOW, HIGH, LOW, LOW, LOW, HIGH, LOW }, //SNARE
+boolean hardCodedSequence[3][3][8] = {
+  {
+    { HIGH, LOW, LOW, LOW, LOW, HIGH, LOW, LOW }, //KICK
+    { HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH }, //HAT
+    { LOW, LOW, HIGH, LOW, LOW, LOW, HIGH, LOW }, //SNARE
+  },
+  {
+    { HIGH, LOW, HIGH, LOW, LOW, LOW, LOW, HIGH }, //KICK
+    { LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, HIGH}, //HAT
+    { LOW, LOW, HIGH, LOW, LOW, LOW, HIGH, LOW }, //SNARE
+  },
+  {
+    { LOW, HIGH, LOW, LOW, HIGH, LOW, LOW, HIGH }, //KICK
+    { HIGH, HIGH, HIGH, LOW, HIGH, HIGH, HIGH, LOW }, //HAT
+    { LOW, LOW, HIGH, LOW, LOW, LOW, HIGH, LOW }, //SNARE
+  }
 };
 
 int midiNotes[3] = { 36 /*kick = C1*/, 42 /*hat = F#1*/, 38 /*sneuh = D1*/ };
@@ -19,18 +33,16 @@ int midiNotes[3] = { 36 /*kick = C1*/, 42 /*hat = F#1*/, 38 /*sneuh = D1*/ };
 void setup() {
   Serial.begin(9600);
   // put your setup code here, to run once:
-
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   playSequenceMIDI();
-
 }
 
 void playSequenceMIDI() {
-  grooveAmount = analogRead(A14)/10; //fix this later, it's kinda janky
-  
+  grooveAmount = analogRead(A14) / 10; //fix this later, it's kinda janky
+
   if (millis() > nextStepTime) { //THIS IS THE MOMENT time passes onto next step
     //turn things off
     for (int i = 0; i < 3; i++) {
@@ -40,21 +52,21 @@ void playSequenceMIDI() {
     //increment
     currentStep = currentStep + 1;
     if ((currentStep % 2) == 0) {
-      nextStepTime = millis()+grooveAmount+tempo;
+      nextStepTime = millis() + grooveAmount + tempo;
     }
     if ((currentStep % 2) != 0) {
-      nextStepTime = millis()-grooveAmount+tempo;
+      nextStepTime = millis() - grooveAmount + tempo;
     }
     if (currentStep > 7) { //once step 8 is done...
       currentStep = 0; //it will reset back to step 1.
     }
-     // lastStepTime = millis();
+    // lastStepTime = millis();
 
 
     //calculate...
     //and set things on
     for (int i = 0; i < 3; i++) {
-      if (hardCodedSequence[i][currentStep] == true) {
+      if (hardCodedSequence[0][i][currentStep] == true) {
         usbMIDI.sendNoteOn(midiNotes[i], 127, 1);
       }
     }
