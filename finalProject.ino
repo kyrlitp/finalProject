5a6555cb4639536d42fe257d436dccee03cfcff7
Jalen <kyrlitp@gmail.com>
2018-11-18 15:57:28 -0500

    Update finalProject.ino
    (lastChannelButtonState and channelButtonState array increases in value aren't needed.)
    
    Added usbMIDI capabilities - - for now it can just read the hi-hat.
    
    Detecting "tempo" through the average time it takes to go through four hits of the hi-hat. Yes, there's an inherent flaw in that there's a possibility of those four hits being irregular (2 eighth notes, 2 quarter notes). Looking to see if this is a desirable quirk in the program... maybe the grooves will be more interesting!
    
    Next up I need to see how I can get the usbMIDI.read to only look for midi notes (for kick, hat and snare) at intervals dictated by "tempo", and then put those values into arrays.

--- finalProject.ino
+++ finalProject.ino
@@ -1,13 +1,21 @@
-int tempo = 200; //this is in ms
-boolean lastChannelButtonState[1] = { LOW };
-boolean channelButtonState[1] = { LOW }; 
+int tempo = 600; //this is in ms
+boolean lastChannelButtonState[2] = { LOW, LOW };
+boolean channelButtonState[2] = { LOW, LOW };
 int randomButton1 = 11;
 int randomSequence = 0;
+int groove = 0;
 int grooveAmount = 50;
 int currentStep = 0;
 int isEvenBeat = 0;
 unsigned long lastStepTime = 0;
 unsigned long nextStepTime = 0;
+int timeAcross4Hats = 0;
+int ledPin = 13;
+int tempoDetect = 0;
+unsigned long beginTempoDetect = 0;
+unsigned long endTempoDetect = 0;
+int tempoDetected = 0;
+
 // int acceptableMidiValues[] = {} // need to limit this to 36 to 51 (C1 to D#2)
 
 boolean hardCodedSequence[3][3][8] = {
@@ -33,16 +41,68 @@ int midiNotes[3] = { 36 /*kick = C1*/, 42 /*hat = F#1*/, 38 /*sneuh = D1*/ };
 
 //
 void setup() {
+  // list of inputs and outputs
   Serial.begin(9600);
-  // put your setup code here, to run once:
   pinMode(randomButton1, INPUT);
+  pinMode(ledPin, OUTPUT);
+
+  // usbMIDI setup
+  usbMIDI.setHandleNoteOff(tempoCalculator);
+  usbMIDI.setHandleNoteOn(onNoteOn);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  seqRandomizer();
-  playSequenceMIDI();
+  usbMIDI.read(); //allows onNoteOn and onNoteOff to work.
+  if (tempoDetected == 1) { // we need to make sure we actually have a tempo before we do anything. (duh.)
+    seqRandomizer();
+    playSequenceMIDI();
+  }
 }
+
+
+
+void tempoCalculator(byte channel, byte note, byte velocity) {
+  digitalWrite(ledPin, LOW); //any note off turns off led
+  //  Serial.println(note);
+  //  Serial.println(velocity);
+  //  Serial.println("OFF");
+  if (tempoDetected == 0) {
+    if (note == 42) {
+      Serial.println("THAT'S THE HAT!!!");
+      tempoDetect++;
+      if (tempoDetect == 1) {
+        beginTempoDetect = millis();
+        Serial.print(beginTempoDetect);
+      }
+      if (tempoDetect == 4) {
+        endTempoDetect = (millis() - beginTempoDetect);
+        Serial.print(endTempoDetect);
+        Serial.print(" milliseconds across 4 hat hits.");
+        tempoDetected = 1;
+      }
+      tempo = (endTempoDetect / 4);
+      groove = (tempo / 2);
+    }
+  }
+}
+
+void onNoteOn(byte channel, byte note, byte velocity) {
+  digitalWrite(ledPin, HIGH); //any note on turns on led
+  //  Serial.println(note);
+  //  Serial.println(velocity);
+  //  Serial.println("ON");
+//  if (tempoDetected == 1) {
+//    if 
+//  }
+
+}
+
+void arrayStorePattern() {
+
+}
+
 
 void seqRandomizer() {
   lastChannelButtonState[0] = channelButtonState[0];
@@ -50,13 +110,15 @@ void seqRandomizer() {
 
   //exact moment when button changes state
   if (channelButtonState[0] == HIGH && lastChannelButtonState[0] == LOW) {
-    randomSequence = random(0,3); //syntax: min (inclusive), max (exclusive)
+    randomSequence = random(0, 3); //syntax: min (inclusive), max (exclusive)
     Serial.println(randomSequence);
   }
 }
 
 void playSequenceMIDI() {
-  grooveAmount = analogRead(A14) / 10; //fix this later, it's kinda janky
+  grooveAmount = analogRead(A14); //NEEDS TO GET MAPPED
+  grooveAmount = map(grooveAmount, 0, 1023, 0, groove);
+  
 
   if (millis() > nextStepTime) { //THIS IS THE MOMENT time passes onto next step
     //turn things off
